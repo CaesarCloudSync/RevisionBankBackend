46677307c51b3b90cf176d1c47dc1c77bc6d2073
CaesarCloudSync <113305679+RevisionBank@users.noreply.github.com>
2023-04-14 20:25:45 +0100

    email quick schedule

--- test2.py
+++ test2.py
@@ -4,5 +4,7 @@ if __name__ == "__main__":
     email = "amari.lawal@gmail.com"
     message = "Hello World"
     subject = f"RevisionBank Send Now"
-    import req
-    #RaspEmail.send(**{"email":email,"message":message,"subject":subject})
+    import requests
+    #requests.post("http://192.168.0.23:7860/revisionbanksendemail",json={"email":"revisionbankedu@gmail.com","subject":"RevisionBank New User","message":"RevisionBank New User"})
+    #RaspEmail.send(email = email,subject = subject,message = message,attachment = None)
+    #RaspEmail.send(**{"email":"revisionbankedu@gmail.com","subject":"RevisionBank New User","message":"RevisionBank New User"})
