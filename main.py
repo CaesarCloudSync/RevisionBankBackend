4be6e8dc399621d1c6c8952638d3a39d6fceb0de
CaesarCloudSync <113305679+RevisionBank@users.noreply.github.com>
2023-05-11 20:45:13 +0100

    getrevisioncardsws maybe connection works

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ from datetime import datetime
 from PIL import Image, ImageOps
 from io import BytesIO
 import base64
+from websockets.exceptions import ConnectionClosedError
 import json
 import stripe
 import jwt
@@ -411,33 +412,31 @@ async def getrevisioncardsws(websocket: WebSocket):
 
     try:
         while True:
-            authinfo = await websocket.receive_json()
-            authorization = authinfo["headers"]["Authorization"]
-            current_user = secure_decode(authorization.replace("Bearer ",""))["email"]
-            if current_user:
-                try:
-                    email_exists = importcsv.db.accountrevisioncards.find_one({"email":current_user})
-                    if email_exists:  # Checks if email exists
-                        user_revision_cards = list(importcsv.db.accountrevisioncards.find({"email": current_user}))[0]
-                        del user_revision_cards["_id"],user_revision_cards["email"]
-                        #return StreamingResponse(iter_df(user_revision_cards), media_type="application/json")
-                        #return user_revision_cards
-                        for revisioncard in user_revision_cards["revisioncards"]:
-                            revisioncard.update({"revisionscheduleinterval":user_revision_cards["revisionscheduleinterval"],"sendtoemail":user_revision_cards["sendtoemail"]})
-                            await websocket.send_json(json.dumps(revisioncard)) # sends the buffer as bytes
-                    elif not email_exists:
-                        await websocket.send_json(json.dumps({"message":"No revision cards"}))
-                        #return {"message":"No revision cards"} # Send in shape of data
-                except Exception as ex:
-                    return {f"error":f"{type(ex)},{str(ex)}"}
-            elif not current_user:
-                await websocket.send_json(json.dumps({"message":"No user."}))
             
-                    
-
-
-    except WebSocketDisconnect:
-        print("Client disconnected")
+                authinfo = await websocket.receive_json()
+                #print(authinfo)
+                authorization = authinfo["headers"]["Authorization"]
+                current_user = secure_decode(authorization.replace("Bearer ",""))["email"]
+                if current_user:
+                    try:
+                        email_exists = importcsv.db.accountrevisioncards.find_one({"email":current_user})
+                        if email_exists:  # Checks if email exists
+                            user_revision_cards = list(importcsv.db.accountrevisioncards.find({"email": current_user}))[0]
+                            del user_revision_cards["_id"],user_revision_cards["email"]
+                            #return StreamingResponse(iter_df(user_revision_cards), media_type="application/json")
+                            #return user_revision_cards
+                            for revisioncard in user_revision_cards["revisioncards"]:
+                                revisioncard.update({"revisionscheduleinterval":user_revision_cards["revisionscheduleinterval"],"sendtoemail":user_revision_cards["sendtoemail"]})
+                                await websocket.send_json(json.dumps(revisioncard)) # sends the buffer as bytes
+                        elif not email_exists:
+                            await websocket.send_json(json.dumps({"message":"No revision cards"}))
+                            #return {"message":"No revision cards"} # Send in shape of data
+                    except Exception as ex:
+                        return {f"error":f"{type(ex)},{str(ex)}"}
+                elif not current_user:
+                    await websocket.send_json(json.dumps({"message":"No user."}))
+    except ConnectionClosedError as cex:
+        await websocket.send_json(json.dumps({"error":f"{type(cex)},{cex}"}))
 
 
 
