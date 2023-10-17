673c32bbd57f350c1fbf482ba8e911d107a21108
amari <amari.lawal@gmail.com>
2023-10-17 16:29:59 +0100

    RevisionBank Forgot Password improved.

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 docker build -t palondomus/revisionbankbackend .
 docker push palondomus/revisionbankbackend
-docker run -it -p 8000:8000 palondomus/revisionbankbackend 
 git add .
 git commit -m "$1"
 git push origin master:main
+docker run -it -p 8000:8000 palondomus/revisionbankbackend 
