477f760846cbe10df5f1c367f36325374c9f0ee2
amari <amari.lawal@gmail.com>
2023-10-26 10:20:54 +0100

    SQL Finisehd. But not good websocket management

--- build_app.sh
+++ build_app.sh
@@ -3,4 +3,4 @@ docker push palondomus/revisionbankbackendsql
 git add .
 git commit -m "$1"
 git push origin
-docker run -it -p 8000:8000 palondomus/revisionbankbackendsql
+docker run -it -p 8080:8080 palondomus/revisionbankbackendsql
