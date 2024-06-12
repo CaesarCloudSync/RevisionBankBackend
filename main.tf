73b9021a31dd0b020da65a2ef9cfa490ebc2c52f
amari <amari.lawal@gmail.com>
2024-06-12 22:39:23 +0100

    fixed manage add image to set add image as true

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "google_cloud_run_service" "run_service" {
   template {
     spec {
       containers {
-        image = "palondomus/revisionbankbackendsql:13"
+        image = "palondomus/revisionbankbackendsql:14"
       }
     }
   }
