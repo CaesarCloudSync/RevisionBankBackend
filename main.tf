4ea3bca29f5359c841649d558dbf347b0359084f
Amari <amari.lawal@gmail.com>
2025-09-25 10:26:21 +0100

    docker compose and fixed db

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "google_cloud_run_service" "run_service" {
   template {
     spec {
       containers {
-        image = "palondomus/revisionbankbackend:34"
+        image = "palondomus/revisionbankbackend:35"
       }
     }
   }
